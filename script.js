94618316ef04b88b373679c6c32946bbf5fdee29
damiralymbekuulu <damir.alymbek@gmail.com>
2022-03-13 20:25:00 +0600

    feat: added google sheets url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const url = "https://script.google.com/macros/s/AKfycbyfrJn36jzKoM5P8o1dfrxcv77Gi2ZQrjSXwRnEJ4Akld-oAELrt-o525J20HmXT2pzeA/exec";
+const url = "https://script.google.com/macros/s/AKfycbxkzZE5AJry4cXomSYR8hb2cRpmqZvX6awdB5E6n2iDsptSExWmv9fEQ6edUzSvOMznFw/exec";
 const form  = document.forms["google-sheet"];
 form.addEventListener('submit', e => { 
    console.log("hello");
@@ -7,6 +7,16 @@ form.addEventListener('submit', e => {
     fetch(url, { method: 'POST', body: new FormData(form)})
         .then(response => alert("Спасибо за участие в заполнении Анкеты!"))
         .catch(error => console.error('Error!', error.message))
-         
 });
 
+function submitForm() {
+    // Get the first form with the name
+    // Hopefully there is only one, but there are more, select the correct index
+    var frm = document.getElementsByName('contact-form')[0];
+    frm.submit(); // Submit
+    frm.reset();  // Reset
+    return false; // Prevent page refresh
+ }
+
+
+ document.getElementById('blabla').value = ''
